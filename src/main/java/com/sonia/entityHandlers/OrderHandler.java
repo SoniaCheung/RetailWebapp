e3ed3c9f64330b3302bd8d964cbd48891a77b95f
sonia <soniacym@gmail.com>
2018-08-10 20:40:04 +0800

    Added OrderHandler in entityHandlers folder for calculating the total price of an order

---
+++ src/main/java/com/sonia/entityHandlers/OrderHandler.java
@@ -0,0 +1,24 @@
+package com.sonia.entityHandlers;
+
+import java.util.List;
+
+import com.sonia.entities.Order;
+import com.sonia.entities.OrderedProduct;
+
+public class OrderHandler {
+
+	public double calculateOrderTotalCost(Order order) {
+		
+		double result = 0.0;
+		List<OrderedProduct> orderedProductList = order.getOrderedProductList();
+		
+		for(OrderedProduct product : orderedProductList) {
+			result += product.getPrice() * product.getQuantity();
+		}
+		
+		double couponDiscountRate = order.getCoupon().getCouponRate();
+		
+		return result * couponDiscountRate;
+	}
+
+}
